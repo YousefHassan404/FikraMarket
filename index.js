e1f9dc6bed58eb6acbc96f579a514ecb1075ddf7
Youssef Hassan <145775122+YousefHassan404@users.noreply.github.com>
2025-05-07 16:03:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-mongoose.connect('mongodb://localhost:27017/FikraMarket')
+mongoose.connect('mongodb://54.87.27.217:27017/FikraMarket')
   .then(() => console.log('تم الاتصال بـ MongoDB'))
   .catch(err => console.error('خطأ في اتصال MongoDB:', err));
 
